26cea81b5d7ee67b68513d62b4e666912deed917
Jongok Yoon <bak01076@gmail.com>
2022-01-14 14:46:56 +0100

    initial commit with adding antd, react-router-dom

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
